06d894c53f1df0862ed3157f384250430aba5904
Shahriaz Kabir <shahriazkobir1994@hotmail.com>
2024-09-18 22:37:18 +0600

    add sticky mode

--- components/navigation/top-navigation-bar.tsx
+++ components/navigation/top-navigation-bar.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Apple } from 'lucide-react';
 
+import { cn } from '@/lib/utils';
+
 import BrandLogo from './brand-logo';
 import DesktopMenu from './desktop-menu';
 import MenuToggle from './menu-toggle';
 import ProfileDropdown from './profile-dropdown';
 import ToggleDarkMode from './toggle-dark-mode';
 
-function TopNavigationBar() {
+function TopNavigationBar({ sticky = false }: { sticky?: boolean }) {
   return (
-    <header className="border-grey flex items-center justify-between border-b px-3 py-2 shadow-sm">
+    <header
+      className={cn(
+        'border-grey flex items-center justify-between border-b px-3 py-2 shadow-sm',
+        sticky ? 'sticky top-0' : ''
+      )}
+    >
       <div className="flex">
         <MenuToggle />
         <BrandLogo appLogo={<Apple className="size-5" />} appName="UI" />
