2b23889b027cd0e48aced9820fb8f3a315ca7ac9
Shahriaz Kabir <shahriazkobir1994@hotmail.com>
2024-09-28 18:36:12 +0600

    feat: pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+import withSerwistInit from '@serwist/next';
 
-export default nextConfig;
+// You may want to use a more robust revision to cache
+// files more efficiently.
+// A viable option is `git rev-parse HEAD`.
+const revision = crypto.randomUUID();
+
+const withSerwist = withSerwistInit({
+  cacheOnNavigation: true,
+  reloadOnOnline: true,
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js',
+  additionalPrecacheEntries: [{ url: '/~offline', revision }],
+});
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withSerwist(nextConfig);
